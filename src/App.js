5fba81aff209cd9a4a63c7f51ef55a6df476f95d
nitinsahu19 <sahunitin900@gmail.com>
2023-10-28 17:52:54 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,81 +1,65 @@
+import React, { useState } from "react";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 
 const App = () => {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       title: "Toilet paper",
       amount: 98.23,
       date: new Date(2023, 7, 14),
       location: "Jaipur",
+      id: 1,
     },
     {
       title: "Car Insurance",
       amount: 323.32,
       date: new Date(2023, 4, 13),
       location: "Sawai-Madhopur",
+      id: 2,
     },
     {
       title: "New TV",
       amount: 3432.33,
       date: new Date(2023, 9, 4),
       location: "Ajmer",
+      id: 3,
     },
     {
       title: "Cycle",
       amount: 5500,
       date: new Date(2023, 11, 9),
       location: "Tonk",
+      id: 4,
     },
     {
       title: "Speaker",
       amount: 2550,
       date: new Date(2023, 11, 8),
       location: "Shivdaspura",
+      id: 5,
     },
-  ];
+  ]);
+
+  const deleteExpenseHandler = (id) => {
+    const updatedExpenses = expenses.filter((expense) => expense.id !== id);
+    setExpenses(updatedExpenses);
+  };
 
   return (
     <div>
       <h2>Expense Tracker</h2>
-      {expenses.map((expense, index) => (
+      {expenses.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           ExpenseTitle={expense.title}
           ExpenseAmount={expense.amount}
           ExpenseDate={expense.date}
-          x
           locationOfExpenditure={expense.location}
+          onDelete={() => deleteExpenseHandler(expense.id)}
         />
       ))}
     </div>
   );
-}
+};
 
 export default App;
-
-{
-  /* <ExpenseItem
-  ExpenseTitle={expenses[0].title}
-  ExpenseAmount={expenses[0].amount}
-  ExpenseDate={expenses[0].date}
-></ExpenseItem>
-<ExpenseItem
-  ExpenseTitle={expenses[1].title}
-  ExpenseAmount={expenses[1].amount}
-  ExpenseDate={expenses[1].date}
-></ExpenseItem>
-<ExpenseItem
-  ExpenseTitle={expenses[2].title}
-  ExpenseAmount={expenses[2].amount}
-  ExpenseDate={expenses[2].date}
-></ExpenseItem>
-<ExpenseItem
-  ExpenseTitle={expenses[3].title}
-  ExpenseAmount={expenses[3].amount}
-  ExpenseDate={expenses[3].date}
-></ExpenseItem>
-<ExpenseItem
-  ExpenseTitle={expenses[4].title}
-  ExpenseAmount={expenses[4].amount}
-  ExpenseDate={expenses[4].date}
-></ExpenseItem> */
-}
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,45 @@
 import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate";
-import ExpenseDetails from "./ExpenseDetails";
-import Card from "../UI/Card";
+import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const {
+    ExpenseTitle,
+    ExpenseAmount,
+    ExpenseDate,
+    locationOfExpenditure,
+    onDelete,
+  } = props;
   
   return (
-    <Card className="expense-item">
-      <ExpenseDate ExpenseDate={props.ExpenseDate} />
-      <ExpenseDetails
-        ExpenseTitle={props.ExpenseTitle}
-        ExpenseAmount={props.ExpenseAmount}
-        locationOfExpenditure={props.locationOfExpenditure}
-      />
-    </Card>
+    <div className="expense-item">
+      <h3>{ExpenseTitle}</h3>
+      <p>Amount: {ExpenseAmount}</p>
+      <p>Date: {ExpenseDate.toDateString()}</p>
+      <p>Location: {locationOfExpenditure}</p>
+      <button onClick={onDelete}>Delete</button>
+    </div>
   );
 };
 
 export default ExpenseItem;
+
+// <Card className="expense-item">
+//   <ExpenseDate ExpenseDate={props.ExpenseDate} />
+//   <ExpenseDetails
+//     ExpenseTitle={props.ExpenseTitle}
+//     ExpenseAmount={props.ExpenseAmount}
+//     locationOfExpenditure={props.locationOfExpenditure}
+//   />
+//   <button onClick={handleDeleteClick}>DELETE</button>
+// </Card>
+
+// const Delete = () => {
+//   const filteredExpenses = props.expenses.filter(
+//     (expense) => expense.title !== props.id
+//   );
+//   setExpenses(filteredExpenses);
+// };
+
+// import ExpenseDate from "./ExpenseDate";
+// import ExpenseDetails from "./ExpenseDetails";
+// import Card from "../UI/Card";
