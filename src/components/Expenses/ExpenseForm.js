3c9a25b6d48cc1db6db3fd4460219e22242f897a
nitinsahu19 <sahunitin900@gmail.com>
2023-10-29 23:34:09 +0530

    onChange event-handler

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,31 +1,81 @@
-import React from 'react';
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const getTitle = (event) =>  {
-            console.log(event.target.value)
-        }
-    
+  // const getTitle = (event) =>  {
+  //         console.log(event.target.value)
+  //     }
+
+  const [title, setTitle] = useState(" ");
+  const [amount, setamount] = useState(" ");
+  const [date, setdate] = useState(" ");
+  const [location, setlocation] = useState(" ");
+
+  const titleHandler = (event) => {
+    setTitle(event.target.value);
+    console.log(title);
+  };
+
+  const amountHandler = (event) => {
+    setamount(event.target.value);
+    console.log(amount)
+  };
+
+  const dateHandler = (event) => {
+    setdate(event.target.value);
+    console.log(date)
+  };
+
+  const locationHandler = (event) => {
+    setlocation(event.target.value);
+    console.log(location)
+  };
 
   return (
     <form className="expense-form-container">
-      <label htmlFor='' className="expense-label">
+      <label htmlFor="" className="expense-label">
         Expense title:
-        <input type="text" name="title" id='title' className="expense-input" onChange={getTitle} />
+        <input
+          type="text"
+          name="title"
+          id="title"
+          className="expense-input"
+          onChange={titleHandler}
+        />
       </label>
-      <label htmlFor='amount' className="expense-label">
+      <label htmlFor="amount" className="expense-label">
         Expense amount:
-        <input type="text" name="amount" id='amount' className="expense-input" />
+        <input
+          type="text"
+          name="amount"
+          id="amount"
+          className="expense-input"
+          onChange={amountHandler}
+        />
       </label>
-      <label htmlFor='date' className="expense-label">
+      <label htmlFor="date" className="expense-label">
         Expense date:
-        <input type="date" name="date" id='date' className="expense-input" />
+        <input
+          type="date"
+          name="date"
+          id="date"
+          className="expense-input"
+          onChange={dateHandler}
+        />
       </label>
-      <label htmlFor='location' className="expense-label">
+      <label htmlFor="location" className="expense-label">
         Location:
-        <input type="text" name="location" id='location' className="expense-input" />
+        <input
+          type="text"
+          name="location"
+          id="location"
+          className="expense-input"
+          onChange={locationHandler}
+        />
       </label>
-      <button type="button" className="add-expense-button">Add Expense</button>
+      <button type="button" className="add-expense-button">
+        Add Expense
+      </button>
     </form>
   );
 };
