4e1d5798f907242fd60fa773cbf050fcec686d11
nitinsahu19 <sahunitin900@gmail.com>
2023-10-31 21:19:56 +0530

    updated to custom wrapper-container

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,53 +2,68 @@ import "./ExpenseItem.css";
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
-// import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDetails from "./ExpenseDetails";
 import ExpenseForm from "./ExpenseForm";
 import ExpenseFilter from "./ExpenseFilter";
 
 const ExpenseItem = (props) => {
-  const {
-    ExpenseTitle,
-    ExpenseAmount,
-    ExpenseDate,
-    locationOfExpenditure,
-    onDelete,
-  } = props;
+  // const {
+  //   ExpenseTitle,
+  //   ExpenseAmount,
+  //   ExpenseDate,
+  //   locationOfExpenditure,
+  //   onDelete,
+  // } = props;
 
-  const [title, setTitle] = useState(ExpenseTitle);
+  const [title, setTitle] = useState(props.ExpenseTitle);
   const clickHandler = () => {
     setTitle("Updated!!");
   };
 
-  const [amount, setAmount] = useState(ExpenseAmount);
+  const [amount, setAmount] = useState(props.ExpenseAmount);
   const expenseHandler = () => {
-    setAmount("$100");
+    setAmount("100");
   };
 
-  const [filteredYear, setFilteredYear] = useState("2020");
+  // const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
+  // const filterChangeHandler = (selectedYear) => {
+  //   setFilteredYear(selectedYear);
+  // };
 
   return (
-    <div className="expense-item">
-      {/* <ExpenseFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      /> */}
-      <h3>{title}</h3>
-      <p>Amount: {amount}</p>
+    // <div className="expense-item">
+    //   <h3>{title}</h3>
+    //   <p>Amount: {amount}</p>
+    //   <button onClick={expenseHandler}>Update</button>
+    //   <p>Date: {ExpenseDate.toDateString()}</p>
+    //   <p>Location: {locationOfExpenditure}</p>
+    //   <button onClick={onDelete}>Delete</button>
+    //   <button onClick={clickHandler}>Change title</button>
+    // </div>
+
+    <Card className="expense-item">
+      <ExpenseDate ExpenseDate={props.ExpenseDate} />
+      <ExpenseDetails
+        ExpenseTitle={title}
+        ExpenseAmount={amount}
+        locationOfExpenditure={props.locationOfExpenditure}
+      />
       <button onClick={expenseHandler}>Update</button>
-      <p>Date: {ExpenseDate.toDateString()}</p>
-      <p>Location: {locationOfExpenditure}</p>
-      <button onClick={onDelete}>Delete</button>
+      <button onClick={props.onDelete}>DELETE</button>
       <button onClick={clickHandler}>Change title</button>
-    </div>
+    </Card>
   );
 };
 
 export default ExpenseItem;
+
+{
+  /* <ExpenseFilter
+  selected={filteredYear}
+  onChangeFilter={filterChangeHandler}
+/> */
+}
 
 // const Delete = () => {
 //   const filteredExpenses = props.expenses.filter(
@@ -60,13 +75,3 @@ export default ExpenseItem;
 // import ExpenseDate from "./ExpenseDate";
 // import ExpenseDetails from "./ExpenseDetails";
 // import Card from "../UI/Card";
-
-// <Card className="expense-item">
-//   { <ExpenseDate ExpenseDate={ExpenseDate} /> }
-//   { <ExpenseDetails
-//     ExpenseTitle={title}
-//     ExpenseAmount={amount}
-//     locationOfExpenditure={locationOfExpenditure}
-//   /> }
-//   <button onClick={onDelete}>DELETE</button>
-// </Card>
