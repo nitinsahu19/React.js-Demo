49a1d905e2fe57c68af21d8d9dcad12fe0e67746
nitinsahu19 <sahunitin900@gmail.com>
2023-11-01 11:40:05 +0530

    filter updated

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import NewExpense from "./components/Expenses/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 import ExpenseFilter from "./components/Expenses/ExpenseFilter";
 
-const App = () => {
+const App = (props) => {
   const [expenses, setExpenses] = useState([
     {
       title: "Toilet paper",
       amount: 98.23,
-      date: new Date(2023, 7, 14),
+      date: new Date(2020, 7, 14),
       location: "Jaipur",
       id: 1,
     },
     {
       title: "Car Insurance",
       amount: 323.32,
-      date: new Date(2023, 4, 13),
+      date: new Date(2021, 4, 13),
       location: "Sawai-Madhopur",
       id: 2,
     },
@@ -31,16 +31,86 @@ const App = () => {
     {
       title: "Cycle",
       amount: 5500,
-      date: new Date(2023, 11, 9),
+      date: new Date(2022, 11, 9),
       location: "Tonk",
       id: 4,
     },
     {
       title: "Speaker",
       amount: 2550,
-      date: new Date(2023, 11, 8),
+      date: new Date(2019, 11, 8),
       location: "Shivdaspura",
       id: 5,
+    },
+    {
+      title: "Laptop",
+      amount: 1200,
+      date: new Date(2021, 10, 15),
+      location: "Delhi",
+      id: 6,
+    },
+    {
+      title: "Groceries",
+      amount: 120.5,
+      date: new Date(2022, 10, 20),
+      location: "Mumbai",
+      id: 7,
+    },
+    {
+      title: "Restaurant Dinner",
+      amount: 85.5,
+      date: new Date(2029, 10, 25),
+      location: "Bangalore",
+      id: 8,
+    },
+    {
+      title: "Movie Tickets",
+      amount: 30,
+      date: new Date(2020, 10, 27),
+      location: "Chennai",
+      id: 9,
+    },
+    {
+      title: "Laptop",
+      amount: 1200,
+      date: new Date(2021, 10, 15),
+      location: "Delhi",
+      id: 10,
+    },
+    {
+      title: "Groceries",
+      amount: 120.5,
+      date: new Date(2022, 10, 20),
+      location: "Mumbai",
+      id:11,
+    },
+    {
+      title: "Toilet paper",
+      amount: 98.23,
+      date: new Date(2020, 7, 14),
+      location: "Jaipur",
+      id: 12,
+    },
+    {
+      title: "Car Insurance",
+      amount: 323.32,
+      date: new Date(2021, 4, 13),
+      location: "Sawai-Madhopur",
+      id: 13,
+    },
+    {
+      title: "New TV",
+      amount: 3432.33,
+      date: new Date(2022, 9, 4),
+      location: "Ajmer",
+      id: 14,
+    },
+    {
+      title: "Cycle",
+      amount: 5500,
+      date: new Date(2022, 11, 9),
+      location: "Tonk",
+      id: 15,
     },
   ]);
 
@@ -57,19 +127,19 @@ const App = () => {
     setExpenses(updatedExpenses);
   };
 
-  const [filteredYear, setFilteredYear] = useState("2020");
+  // const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
+  // const filterChangeHandler = (selectedYear) => {
+  //   setFilteredYear(selectedYear);
+  // };
 
   return (
     <div>
       <h2>Expense Tracker</h2>
-      <ExpenseFilter
+      {/* <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
-      />
+      /> */}
       <Expenses items={expenses} onDelete={deleteExpenseHandler} />
       <NewExpense onAddExpense={addEventHandler} />
     </div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,35 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import App from "../../App";
+import ExpenseFilter from "./ExpenseFilter";
+// import App from "../../App";
 
 const Expenses = (props) => {
-  return props.items.map((expense) => (
-    <ExpenseItem
-      key={expense.id}
-      ExpenseTitle={expense.title}
-      ExpenseAmount={expense.amount}
-      ExpenseDate={expense.date}
-      locationOfExpenditure={expense.location}
-      onDelete={() => props.onDelete(expense.id)}
-    />
-  ));
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+  console.log(filteredExpenses);
+  // console.log(props.items)
+
+  return (
+    <div>
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          ExpenseTitle={expense.title}
+          ExpenseAmount={expense.amount}
+          ExpenseDate={expense.date}
+          locationOfExpenditure={expense.location}
+          onDelete={() => props.onDelete(expense.id)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Expenses;
